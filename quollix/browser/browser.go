03081203ce9647904174beca711768ba0e5a8992
Christoph Baier <anon@example.com>
2026-07-18 13:14:32 +0200

    add browser plus quollix client aggregation

---
+++ quollix/browser/browser.go
@@ -0,0 +1,164 @@
+package browser
+
+import (
+	"net/http"
+
+	"github.com/quollix/common/browsertest"
+	"github.com/quollix/common/quollix/api"
+	"github.com/quollix/common/quollix/api_client"
+	u "github.com/quollix/common/utils"
+)
+
+type Browser struct {
+	BaseURL string
+	Client  *api_client.QuollixClient
+	Browser *browsertest.Browser
+	Page    *browsertest.Page
+
+	InstalledApps *InstalledAppsPageHelpers
+}
+
+func NewBrowser(baseURL string) (*Browser, error) {
+	browser, err := browsertest.LaunchBrowser()
+	if err != nil {
+		return nil, u.Logger.NewError(err.Error())
+	}
+
+	page, err := browser.NewPage()
+	if err != nil {
+		_ = browser.Close()
+		return nil, u.Logger.NewError(err.Error())
+	}
+
+	quollixBrowser := &Browser{
+		BaseURL: baseURL,
+		Client:  api_client.NewQuollixClientForRootUrl(baseURL),
+		Browser: browser,
+		Page:    page,
+	}
+	quollixBrowser.InstalledApps = &InstalledAppsPageHelpers{Page: page}
+	return quollixBrowser, nil
+}
+
+func (b *Browser) Close() error {
+	var closeErr error
+	if b.Page != nil {
+		closeErr = b.Page.Close()
+	}
+	if b.Browser != nil {
+		if err := b.Browser.Close(); err != nil && closeErr == nil {
+			closeErr = err
+		}
+	}
+	if closeErr != nil {
+		return u.Logger.NewError(closeErr.Error())
+	}
+	return nil
+}
+
+func (b *Browser) Visit(path string) error {
+	if err := b.Page.Navigate(b.BaseURL + path); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	return nil
+}
+
+func (b *Browser) SyncedLoginWithClient(username, password string) error {
+	if err := b.Visit(api.Paths.FrontendSignIn); err != nil {
+		return err
+	}
+	if err := b.Client.Auth.SignIn(username, password); err != nil {
+		return err
+	}
+	if err := b.syncBrowserCookieFromClient(); err != nil {
+		return err
+	}
+	return b.Visit(api.Paths.FrontendInstalledApps)
+}
+
+func (b *Browser) SyncedLoginWithBrowser(username, password string) error {
+	if err := b.Visit(api.Paths.FrontendSignIn); err != nil {
+		return err
+	}
+	if err := b.Page.MustElement("#sign-in-tab").Click(); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	if err := b.Page.MustElement("#username-input").Input(username); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	if err := b.Page.MustElement("#password-input").Input(password); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	if err := b.Page.DoAndWaitLoad(func() {
+		b.Page.MustElement("#sign-in-button").MustClick()
+	}); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	return b.syncClientCookieFromBrowser()
+}
+
+func (b *Browser) UseAuthCookie(cookie *http.Cookie) error {
+	b.Client.Parent.Cookie = cookie
+	return b.syncBrowserCookieFromClient()
+}
+
+func (b *Browser) ClearSession() error {
+	b.Client.Parent.Cookie = nil
+	return b.clearBrowserCookies()
+}
+
+func (b *Browser) syncBrowserCookieFromClient() error {
+	if err := b.clearBrowserCookies(); err != nil {
+		return err
+	}
+	if b.Client.Parent.Cookie == nil {
+		return nil
+	}
+	if err := b.Page.SetCookie(b.Client.Parent.Cookie, b.BaseURL); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	return nil
+}
+
+func (b *Browser) clearBrowserCookies() error {
+	if err := b.Page.ClearCookies(); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	return nil
+}
+
+func (b *Browser) syncClientCookieFromBrowser() error {
+	cookie, err := b.getAuthCookieFromBrowser()
+	if err != nil {
+		return err
+	}
+	b.Client.Parent.Cookie = cookie
+	return nil
+}
+
+func (b *Browser) getAuthCookieFromBrowser() (*http.Cookie, error) {
+	cookies, err := b.Page.Cookies(b.BaseURL)
+	if err != nil {
+		return nil, u.Logger.NewError(err.Error())
+	}
+	for _, cookie := range cookies {
+		if cookie.Name != api.BrandAppAuthCookieName {
+			continue
+		}
+		return &http.Cookie{ // #nosec G124: tests reconstruct the browser cookie only for local test replay
+			Name:     cookie.Name,
+			Value:    cookie.Value,
+			Path:     cookie.Path,
+			Secure:   cookie.Secure,
+			HttpOnly: cookie.HTTPOnly,
+		}, nil
+	}
+	return nil, u.Logger.NewError("no auth cookie found")
+}
+
+func (b *Browser) OpenInstalledAppsPage() *InstalledAppsPageHelpers {
+	if err := b.Visit(api.Paths.FrontendInstalledApps); err != nil {
+		panic(err)
+	}
+	return b.InstalledApps
+}
---
+++ quollix/browser/installed_apps_page_helpers.go
@@ -0,0 +1,59 @@
+package browser
+
+import (
+	"strings"
+
+	"github.com/quollix/common/browsertest"
+	u "github.com/quollix/common/utils"
+)
+
+type InstalledAppsPageHelpers struct {
+	Page *browsertest.Page
+}
+
+func (p *InstalledAppsPageHelpers) ClickOpenButton(appName string) error {
+	row, err := p.FindRowByAppName(appName)
+	if err != nil {
+		return err
+	}
+
+	accessCell, err := row.Element(".app-access")
+	if err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+
+	openButton, err := accessCell.Element("button.open-btn")
+	if err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+
+	if err := openButton.Click(); err != nil {
+		return u.Logger.NewError(err.Error())
+	}
+	return nil
+}
+
+func (p *InstalledAppsPageHelpers) OpenAppInNewTab(appName string) (*browsertest.Page, error) {
+	waitForNewTab := p.Page.MustWaitOpen()
+	if err := p.ClickOpenButton(appName); err != nil {
+		return nil, err
+	}
+	return waitForNewTab(), nil
+}
+
+func (p *InstalledAppsPageHelpers) FindRowByAppName(appName string) (*browsertest.Element, error) {
+	rows, err := p.Page.Elements("#installed-apps-tbody tr")
+	if err != nil {
+		return nil, u.Logger.NewError(err.Error())
+	}
+	for _, row := range rows {
+		appNameAttr, err := row.Attribute("data-app-name")
+		if err != nil {
+			return nil, u.Logger.NewError(err.Error())
+		}
+		if appNameAttr != nil && strings.TrimSpace(*appNameAttr) == appName {
+			return row, nil
+		}
+	}
+	return nil, u.Logger.NewError("app row not found", "app_name", appName)
+}
